820e9fcc378abb423eb5685a3dad2d633596291b
Guillaume Aubert <aubertg@cpan.org>
2012-09-08 23:07:27 -0400

    Re-licensed as GPLv3.

--- lib/Queue/DBI.pm
+++ lib/Queue/DBI.pm
@@ -1043,10 +1043,16 @@ Thanks to Sergey Bond for suggesting many features added in version 1.8.x
 
 Copyright 2009-2012 Guillaume Aubert.
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of the Artistic License.
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License version 3 as published by the Free
+Software Foundation.
 
-See http://dev.perl.org/licenses/ for more information.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see http://www.gnu.org/licenses/
 
 =cut
 
--- lib/Queue/DBI/Admin.pm
+++ lib/Queue/DBI/Admin.pm
@@ -517,10 +517,16 @@ and complete the features offered by C<Queue::DBI>.
 
 Copyright 2009-2012 Guillaume Aubert.
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of the Artistic License.
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License version 3 as published by the Free
+Software Foundation.
 
-See http://dev.perl.org/licenses/ for more information.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see http://www.gnu.org/licenses/
 
 =cut
 
