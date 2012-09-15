d79ca40c576d570914adddab20c48e2174042d0b
Guillaume Aubert <aubertg@cpan.org>
2012-09-15 19:51:32 -0400

    Released version 2.1.1.

--- lib/Queue/DBI/Element.pm
+++ lib/Queue/DBI/Element.pm
@@ -14,11 +14,11 @@ Queue::DBI::Element - An object representing an element pulled from the queue.
 
 =head1 VERSION
 
-Version 2.1.0
+Version 2.1.1
 
 =cut
 
-our $VERSION = '2.1.0';
+our $VERSION = '2.1.1';
 
 
 =head1 SYNOPSIS
