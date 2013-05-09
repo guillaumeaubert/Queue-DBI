2ffd31d3fa9ef61af1022fb658e60f6b1f098593
Guillaume Aubert <aubertg@cpan.org>
2013-05-08 21:24:00 -0400

    Updated copyright year.

--- lib/Queue/DBI.pm
+++ lib/Queue/DBI.pm
@@ -1108,7 +1108,7 @@ Thanks to Sergey Bond for suggesting many features added in version 1.8.x
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009-2012 Guillaume Aubert.
+Copyright 2009-2013 Guillaume Aubert.
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License version 3 as published by the Free
--- lib/Queue/DBI/Admin.pm
+++ lib/Queue/DBI/Admin.pm
@@ -996,7 +996,7 @@ and complete the features offered by L<Queue::DBI>.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009-2012 Guillaume Aubert.
+Copyright 2009-2013 Guillaume Aubert.
 
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License version 3 as published by the Free
